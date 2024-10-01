32dbd0595b994569f95d1473977a01b054b90210
g4ze <bhaiyajionline17@gmail.com>
2024-10-01 22:37:54 +0530

    font relocated

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,12 +6,12 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../../public/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../../public/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
