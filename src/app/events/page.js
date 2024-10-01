51ac600543eea29a74089cf7f2e09a9d2fe2cac6
g4ze <bhaiyajionline17@gmail.com>
2024-10-01 23:48:38 +0530

    deprecate features

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,4 +1,4 @@
 export default function EventsPage() {
     return <h1>GDG Events</h1>;
   }
-  
+  // THIS PAGE SHOULD BE DEPRECATED
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,4 +1,4 @@
 export default function EventsPage() {
     return <h1></h1>;
   }
-  
+  // THIS PAGE SHOULD BE DEPRECATED
