911127290299e1e59502bf643f12cf28c6d3f72f
Saransh Jain <saranshj647@gmail.com>
2024-10-13 15:23:46 +0530

    Correct display

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,26 +1,27 @@
+import React from "react";
+
 const EventCard = ({ event }) => {
-    return (
-      <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
-        <img
-          src={event.poster}
-          alt={event.name}
-          className="w-full h-48 object-cover"
-        />
-        <div className="p-4">
-          <h3 className="text-xl font-bold text-white mb-2">{event.name}</h3>
-          <p className="text-gray-300 text-sm mb-4">{event.shortDesc}</p>
-          <a
-            href={event.communityLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-300"
-          >
-            Get More Info!
-          </a>
-        </div>
+  return (
+    <div className="bg-gray-900 rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300  border-b-4 border-[#32cd32]">
+      <img
+        src={event.poster}
+        alt={event.name}
+        className="w-full h-48 object-cover opacity-90 hover:opacity-100 transition-opacity"
+      />
+      <div className="p-4">
+        <h3 className="text-2xl font-bold text-white mb-2">{event.name}</h3>
+        <p className="text-gray-400 text-sm mb-4">{event.shortDesc}</p>
+        <a
+          href={event.communityLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gradient-to-r from-gray-700 to-gray-800 text-white py-2 px-4 rounded-full hover:bg-gray-900 transition-all"
+        >
+          Learn More
+        </a>
       </div>
-    );
-  };
-  
-  export default EventCard;
-  
+    </div>
+  );
+};
+
+export default EventCard;
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,4 +1,4 @@
-import EventCard from './EventCard';
+import EventCard from "./EventCard";
 
 const Events = ({ events }) => {
   return (
