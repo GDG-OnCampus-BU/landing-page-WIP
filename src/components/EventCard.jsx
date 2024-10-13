b56a7cb6a82f4cddea6109bc41a561252e445088
Saransh Jain <saranshj647@gmail.com>
2024-10-13 21:10:23 +0530

    sample data updated

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const EventCard = ({ event }) => {
   return (
-    <div className="bg-gray-900 rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300  border-b-4 border-[#32cd32]">
+    <div className="bg-gray-900 rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-all duration-300 border-b-4 border-[#32cd32]">
       <img
         src={event.poster}
         alt={event.name}
-        className="w-full h-48 object-cover opacity-90 hover:opacity-100 transition-opacity"
+        className="w-full h-48 object-contain opacity-90 hover:opacity-100 transition-opacity"
       />
       <div className="p-4">
         <h3 className="text-2xl font-bold text-white mb-2">{event.name}</h3>
