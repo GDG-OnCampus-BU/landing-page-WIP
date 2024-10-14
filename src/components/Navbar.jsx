367f6311c050acbf7f0e228ace8f8d98d824d1c6
user.name <avi26022007@gmail.com>
2024-10-14 20:25:49 +0530

    Get Started button is now visible in light mode and correct the text decoration of home button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -86,7 +86,7 @@ export default function  Navbar () {
               <li>
                 <a
                   href="#"
-                  className="block py-2 px-3 text-white  rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                  className=" block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   aria-current="page"
                 >
                   Home
