7185ea56247f08a058f03edafc383a78f176c90e
g4ze <bhaiyajionline17@gmail.com>
2024-10-20 12:11:54 +0530

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 
 export default function Loader() {
+  const public_path = process.env.NEXT_PUBLIC_PUBLIC_PATH
+
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center z-50 bg-gray-900">
       <div className="relative flex items-center justify-center">
         <div className="0 w-full h-full animate-pulse ">
           <Image
-            src="/assets/logo_gdg.png"
+            src={public_path+"/assets/logo_gdg.png"}
             alt="GDG Logo"
             width={40}
             height={20}
