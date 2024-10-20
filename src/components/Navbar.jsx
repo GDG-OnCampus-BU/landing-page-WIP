9bdb821560aae596bbb8dc8853e06a324fc5210b
g4ze <bhaiyajionline17@gmail.com>
2024-10-20 10:29:02 +0530

    image optimiztion for prod

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ export default function Navbar() {
       >
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
-            href="https://flowbite.com/"
+            href=""
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <Image
